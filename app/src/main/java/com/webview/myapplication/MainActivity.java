c0641ff582e796e6eb686c06425bee2f58b2b595
BenGu103 <79404940+BenGu103@users.noreply.github.com>
2026-02-08 16:10:20 +0200

    Update MainActivity.java

--- app/src/main/java/com/webview/myapplication/MainActivity.java
+++ app/src/main/java/com/webview/myapplication/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends Activity {
         });
 
         if (isNetworkAvailable()) {
-            mWebView.loadUrl("https://github.com/bishwassagar");
+            mWebView.loadUrl("https://www.google.com");
         } else {
             mWebView.loadUrl("file:///android_asset/offline.html");
         }
